ae254350db802309488b8a9890d5a3c46a7ca9bd
Jeremy House <jmhouse118@gmail.com>
2025-12-03 10:12:44 -0500

    Fix desktop message

--- src/components/DesktopOnly/DesktopOnly.jsx
+++ src/components/DesktopOnly/DesktopOnly.jsx
@@ -6,12 +6,12 @@ export default function DesktopOnly() {
         <div className="desktop-only">
             <div className="desktop-only__container">
                 <img src={logo} alt="ScoreSync Logo" className="desktop-only__logo" />
-                <div className="desktop-only__dext">
+                <div className="desktop-only__text">
                     <p className="desktop-only__small">Looks like you're on mobile!</p>
 
-                    <h1 className="desktop-only__headline">
+                    <p className="desktop-only__headline">
                         Some things are meant to be viewed on a larger screen
-                    </h1>
+                    </p>
                     <p className="desktop-only__body">
                         ScoreSync editor performs best on a bigger stage. <br />
                         Try logging in from your desktop to start composing
